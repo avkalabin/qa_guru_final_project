f2f16b64e8f362531ba961ca4ec93a08753897c0
Andrei Kalabin <a.kalabin@inbox.ru>
2023-05-20 14:33:52 +0500

    changed LoadPolicy

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -3,7 +3,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
+        "system:properties",
         //"classpath:${env}.properties",
         "classpath:credentials.properties"
 
