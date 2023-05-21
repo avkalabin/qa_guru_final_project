08e363f7669837da5418f5241712dc8c5aae71c3
Andrei Kalabin <a.kalabin@inbox.ru>
2023-05-21 16:17:08 +0500

    updated TOKENS

--- src/test/java/web/testops/helpers/CookieAuth.java
+++ src/test/java/web/testops/helpers/CookieAuth.java
@@ -9,8 +9,8 @@ import static java.lang.String.format;
 public class CookieAuth {
 
     final static public String PROJECT_ID = "2264",
-            X_XSRF_TOKEN = 	"cf1b166e-1cef-4033-bf70-d4ad4f3dd67d",
-            ALLURE_TESTOPS_SESSION = "6fea0f28-885e-4833-b273-def9121c9f97";
+            X_XSRF_TOKEN = 	"4e76948c-7306-423d-aa7f-81506cc9de8c",
+            ALLURE_TESTOPS_SESSION = "99716ac5-bb39-4269-8cad-c943debacf13";
 
     public CookieAuth authWithCookie() {
         open("/favicon.ico");
